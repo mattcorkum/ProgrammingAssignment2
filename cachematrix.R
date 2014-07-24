a3d1bc4ab775d2e8303c8b8037d1d40858c322b4
matt corkum <corkum@gmail.com>
2014-07-24 16:46:30 -0400

    got it working

--- cachematrix.R
+++ cachematrix.R
@@ -1,36 +1,4 @@
 
-## Make Vector function
-makeVector <- function(x = numeric()) {
-        m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
-        }
-        get <- function() x
-        setmean <- function(mean) m <<- mean
-        getmean <- function() m
-        list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
-}
-
-## cachemean function
-cachemean <- function(x, ...) {
-
-  ## look up the mean
-        m <- x$getmean()
-
-        ## if we computed and cached it, then return it
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
-        ## else do a lazy eval of computing the inverse of the matrix data
-        data <- x$get()
-        m <- mean(data, ...)
-        x$setmean(m)
-        m
-}
 
 ## makeCacheMatrix
 
@@ -45,7 +13,7 @@ makeCacheMatrix <- function(x = matrix()) {
     m <<- NULL
   }
   get <- function() x
-  setInv <- function(solve) m <<- solve
+  setInv <- function(x) m <<- solve
   getInv <- function() m
 
    ## create the set and get accessors for this computed inverse
@@ -83,7 +51,7 @@ cacheSolve <- function(x,...) {
   }
 
   ## else compute the inverse, start by fetching the data of the matrix
-  data <- x$get()
+  data <- as.numeric(x$get())
   
   ## invert it
   m <- solve(data, ...)
