9ce7ab973ac6ae134da2857aeaf6713f77ccea97
matt corkum <corkum@gmail.com>
2014-07-24 10:05:48 -0400

    added the functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Make Vector function
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
 }
 
+## cachemean function
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
+}
 
-## Write a short comment describing this function
+## makeCacheMatrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##
+## This function creates a special "matrix" object that can cache its inverse.
+##
+makeCacheMatrix <- function(x = matrix()) {
+
+  ## compute special matrix
+  
+}
+
+## cacheSolve
+##
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed),
+## then the cachesolve should retrieve the inverse from the cache.
+##
+## returns the inverse of the 'x' matrix
+##
+cacheSolve <- function(x,...) {
+
+  ## check and see if cache already has calculated the inverse
+  
+  ## else compute the inverse
+  ##
+  ## if X is a square invertible matrix, then solve(X) returns its inverse.
+  ##
+  ## For this assignment, assume that the matrix supplied is always invertible.
+  ##
+  Xinv <- solve(x)
+
+
+  ## cache the computed inverse 
+
 }
